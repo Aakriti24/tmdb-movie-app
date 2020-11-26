a2780cd62434c5695d011c7e37c700946c4ecdfb
Aakriti Subedi <asubedi@mail.sfsu.edu>
2020-11-26 12:43:17 -0800

    TMDB movie app

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { MoviesModule } from './../pages/movies/movies.module';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Network } from '@ionic-native/network';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    MoviesModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    Network
+  ]
+})
+export class AppModule { }
